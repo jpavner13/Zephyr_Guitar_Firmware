d5a9e799b71ff361470c61976cc3a28256e74a4a
Pierce Avner <jpavner@gmail.com>
2023-06-01 18:15:02 +0000

    Got Blinking working!

--- app/src/main.c
+++ app/src/main.c
@@ -1,23 +1,43 @@
-#include <zephyr/device.h>
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 
-#define SLEEP_TIME 1000
 
-const struct gpio_dt_spec spec =
-    GPIO_DT_SPEC_GET_BY_IDX(DT_NODELABEL(green_led_2), gpios, 0);
+/* 1000 msec = 1 sec */
+#define SLEEP_TIME_MS 1000
+
+/* The devicetree node identifier for the "led0" alias. */
+#define LED0_NODE DT_ALIAS(led0)
+
+/*
+ * A build error on this line means your board is unsupported.
+ * See the sample documentation for information on how to fix this.
+ */
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 int main(void) {
-  int cnt = 0;
+  int ret;
 
-  struct device *dev;
-  dev = device_get_binding(spec.port);
-
-  while (1) {
-    gpio_pin_set(dev, spec.pin, cnt % 2);
-    cnt++;
-    k_msleep(SLEEP_TIME);
+  if (!gpio_is_ready_dt(&led)) {
+    return 0;
   }
 
+  ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+  if (ret < 0) {
+    return 0;
+  }
+
+  while (1) {
+    ret = gpio_pin_toggle_dt(&led);
+    if (ret < 0) {
+      return 0;
+    }
+    k_msleep(SLEEP_TIME_MS);
+  }
   return 0;
 }
