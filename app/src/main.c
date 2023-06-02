2fbde9c63bee524bb09d073ff56b26c844362771
Pierce Avner <jpavner@gmail.com>
2023-06-02 23:48:45 +0000

    Added multi-threading for a blinking LED and and a button reactive LED

--- app/src/main.c
+++ app/src/main.c
@@ -5,7 +5,10 @@
 #include <zephyr/sys/printk.h>
 #include <inttypes.h>
 
-#define SLEEP_TIME_MS 1
+// Define thread stack size and priority
+#define STACKSIZE 1024
+#define BUTTON_PRIORITY 7
+#define BLINK_PRIORITY 7
 
 // INITIALIZE THE USER BUTTON
 #define SW0_NODE DT_ALIAS(sw0)
@@ -17,7 +20,35 @@ static struct gpio_dt_spec onboardLed = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led0), gpio
 // INITIALIZE THE RED LED
 static struct gpio_dt_spec led = GPIO_DT_SPEC_GET_OR(DT_ALIAS(redled), gpios, {0});
 
-int main(void) {
+// Thread 0 Function
+void buttonLED(void)
+{
+  while (1) 
+  {
+    int val = gpio_pin_get_dt(&button);
+
+    if (val >= 0) 
+    {
+      gpio_pin_set_dt(&led, val);
+    }
+  }
+}
+
+// Thread 1 Function
+void blink()
+{
+  while(1)
+  {
+    if((k_uptime_get() % 1000) == 0)
+    {
+      gpio_pin_toggle_dt(&onboardLed);
+    }
+  }
+}
+
+// Main funtion (Basically a thread with priority 1)
+void main()
+{
   // Configure Button as an input
   gpio_pin_configure_dt(&button, GPIO_INPUT);
   gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
@@ -25,16 +56,7 @@ int main(void) {
   // Configure LED's as an output
   gpio_pin_configure_dt(&onboardLed, GPIO_OUTPUT);
   gpio_pin_configure_dt(&led, GPIO_OUTPUT);
-
-  while (1) {
-    int val = gpio_pin_get_dt(&button);
-
-    if (val >= 0) {
-      gpio_pin_set_dt(&onboardLed, val);
-      gpio_pin_set_dt(&led, val);
-    }
-    k_msleep(SLEEP_TIME_MS);
-  }
-
-  return 0;
 }
+
+K_THREAD_DEFINE(buttonLED_thread, STACKSIZE, buttonLED, NULL, NULL, NULL, BUTTON_PRIORITY, 0, 0);
+K_THREAD_DEFINE(blinkLED_thread, STACKSIZE, blink, NULL, NULL, NULL, BLINK_PRIORITY, 0, 0);
